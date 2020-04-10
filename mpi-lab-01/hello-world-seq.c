45c9bafcceebcd69d7ee15822c0a1b65b17ebc9f
Stach <staho.kazmierowski@gmail.com>
2020-04-10 15:35:23 +0200

    small cammit

--- mpi-lab-01/hello-world-seq.c
+++ mpi-lab-01/hello-world-seq.c
@@ -1,4 +1,4 @@
-0/*
+/*
  * A template for the 2016 MPI lab at the University of Warsaw.
  * Copyright (C) 2016, Konrad Iwanicki
  * Further modifications by Krzysztof Rzadca 2018
@@ -16,15 +16,10 @@ int main(int argc, char * argv[])
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
-    
-//    struct timespec spec;
-//    clock_gettime(CLOCK_REALTIME, &spec);
-//    srand(spec.tv_nsec); // use nsec to have a different value across different processes
-    
-//    unsigned t = rand() % 5;
-//    sleep(t);
+
     printf("Hello from thread number %d", myRank);
     if(myRank == MASTER)
     	printf("MASTER: number of tasks is: %d", numProcesses);
+   	MPI_Finalize();
     return 0;
 }
