6eff56193a77d3607f8848eae40759f5c66fe8f1
Stach <staho.kazmierowski@gmail.com>
2020-04-10 20:20:28 +0200

    adding benchmark

--- mpi-lab-01/benchmark.c
+++ mpi-lab-01/benchmark.c
@@ -11,7 +11,10 @@ double benchmark(int size){
 	double startTime;
 	double endTime;
 	double executionTime;
-
+	int numProcesses = 2;
+	int myRank;
+	MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
 	
 	startTime = MPI_Wtime();
 	if(myRank == 0){
@@ -27,20 +30,16 @@ double benchmark(int size){
 }
 
 int main(int argc, char * argv[]){
-int size = 1;
-double time;
-int numProcesses = 2;
-int myRank;
-MPI_Init(&argc, &argv);
-MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
-MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
-for(int i = 0 ; i < MAX_POWER ; i++){
-	size *= 10;
-	for(int j = 0; j < SAMPLE_SIZE ; j++){
-		time = benchmark(size, argc, argv);
-		printf("%d %d %lf\n", j, size, time);
+	int size = 1;
+	double time;
+	MPI_Init(&argc, &argv);
+	for(int i = 0 ; i < MAX_POWER ; i++){
+		size *= 10;
+		for(int j = 0; j < SAMPLE_SIZE ; j++){
+			time = benchmark(size);
+			printf("%d %d %lf\n", j, size, time);
+		}
 	}
-}
-MPI_Finalize();
-return 0;
+	MPI_Finalize();
+	return 0;
 }
