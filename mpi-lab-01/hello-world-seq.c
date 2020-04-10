10754351ffbb057db46837b481123d4340efda63
Stach <staho.kazmierowski@gmail.com>
2020-04-10 15:22:05 +0200

    upgrade of hello-world

--- mpi-lab-01/hello-world-seq.c
+++ mpi-lab-01/hello-world-seq.c
@@ -1,4 +1,4 @@
-/*
+0/*
  * A template for the 2016 MPI lab at the University of Warsaw.
  * Copyright (C) 2016, Konrad Iwanicki
  * Further modifications by Krzysztof Rzadca 2018
@@ -7,6 +7,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
+#include <mpi.h>
 #define MASTER 0
 
 int main(int argc, char * argv[])
@@ -16,7 +17,7 @@ int main(int argc, char * argv[])
 	MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
     
-    struct timespec spec;
+//    struct timespec spec;
 //    clock_gettime(CLOCK_REALTIME, &spec);
 //    srand(spec.tv_nsec); // use nsec to have a different value across different processes
     
