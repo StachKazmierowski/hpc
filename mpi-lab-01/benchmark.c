810448f2d7922652609e5cbba0008a244f4e6bf4
Stach <staho.kazmierowski@gmail.com>
2020-04-10 21:29:38 +0200

    adding benchmark

--- mpi-lab-01/benchmark.c
+++ mpi-lab-01/benchmark.c
@@ -3,46 +3,51 @@
 #include <stdlib.h>
 #include <time.h>
 #include <mpi.h>
-#define MAX_POWER 7
+#define MAX_PAYLOAD
 #define SAMPLE_SIZE 30
 
-double benchmark(int size){
-	void* token;
-	token = malloc(size);
-	double startTime;
-	double endTime;
-	double executionTime;
-	int numProcesses = 2;
-	int myRank;
-	MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
-	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
-	
-	startTime = MPI_Wtime();
-	if(myRank == 0){
-		MPI_Send(token, size, MPI_BYTE, 1, 0, MPI_COMM_WORLD);
-		MPI_Recv(token, size, MPI_BYTE, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	} else {
-		MPI_Recv(token, size, MPI_BYTE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		MPI_Send(token, size, MPI_BYTE, 0, 0, MPI_COMM_WORLD);
-	}
-	endTime = MPI_Wtime();
-	executionTime = endTime - startTime;
-	return executionTime;
+double benchmark(void* buff, int to, int payload){
+    double startTime,endTime;
+
+    startTime = MPI_Wtime();
+    MPI_Send(buff, payload, MPI_BYTE, to, 0, MPI_COMM_WORLD);
+    MPI_Recv(buff, payload, MPI_BYTE, to, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    endTime = MPI_Wtime();
+    return endTime - startTime;
 }
 
 int main(int argc, char * argv[]){
-	int size = 1;
-	double time;
-	printf("befor init");
 	MPI_Init(&argc, &argv);
-	printf("after init");
-	for(int i = 0 ; i < MAX_POWER ; i++){
-		size *= 10;
-		for(int j = 0; j < SAMPLE_SIZE ; j++){
-			time = benchmark(size);
-			printf("%d %d %lf\n", j, size, time);
+	
+	int numProcesses, myRank;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+    int node = myRank/(numProcesses/2);
+    int partnerNode, partnerRank;
+    if(node == 0){
+        partnerNode = 1;
+        partnerRank = myRank + (numProcesses/2);
+    } else {
+        partnerNode = 0;
+        partnerRank = myRank - (numProcesses/2);
+    }
+    
+    int max_payload = MAX_PAYLOAD;
+    void* buff = malloc(max_payload);
+    
+    if(node == 0){	
+		for(int i = 0 ; i < max_payload ; i *= 10){
+            MPI_Recv(buff, i, MPI_BYTE, partnerRank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Send(buff, i, MPI_BYTE, partnerRank, 0, MPI_COMM_WORLD);
+		}
+	} else {
+		for(int i = 0 ; i < max_payload ; i *= 10){
+			double benchmarkTime = benchmark(buff, partnerRank, i);
+			pritnf("%d %d %lf\n", partnerRank, i, benchmarkTime);
 		}
 	}
+	free(buff);
 	MPI_Finalize();
 	return 0;
 }
