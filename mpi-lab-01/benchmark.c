55f3070ba584669cac4b6977d7b3c00fa3172f36
Stach <staho.kazmierowski@gmail.com>
2020-04-10 20:18:30 +0200

    adding benchmark

--- mpi-lab-01/benchmark.c
+++ mpi-lab-01/benchmark.c
@@ -6,16 +6,12 @@
 #define MAX_POWER 7
 #define SAMPLE_SIZE 30
 
-double benchmark(int size, int argc, char* argv[]){
+double benchmark(int size){
 	int token;
 	double startTime;
 	double endTime;
 	double executionTime;
-	int numProcesses = 2;
-	int myRank;
-	MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
-	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
+
 	
 	startTime = MPI_Wtime();
 	if(myRank == 0){
@@ -27,13 +23,17 @@ double benchmark(int size, int argc, char* argv[]){
 	}
 	endTime = MPI_Wtime();
 	executionTime = endTime - startTime;
-   	MPI_Finalize();
 	return executionTime;
 }
 
 int main(int argc, char * argv[]){
 int size = 1;
 double time;
+int numProcesses = 2;
+int myRank;
+MPI_Init(&argc, &argv);
+MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
+MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
 for(int i = 0 ; i < MAX_POWER ; i++){
 	size *= 10;
 	for(int j = 0; j < SAMPLE_SIZE ; j++){
@@ -41,5 +41,6 @@ for(int i = 0 ; i < MAX_POWER ; i++){
 		printf("%d %d %lf\n", j, size, time);
 	}
 }
+MPI_Finalize();
 return 0;
 }
