d89ffd571f54b7f77dd7a68e3913cafb090465df
Stach <staho.kazmierowski@gmail.com>
2020-04-10 20:41:25 +0200

    adding benchmark

--- mpi-lab-01/benchmark.c
+++ mpi-lab-01/benchmark.c
@@ -6,38 +6,38 @@
 #define MAX_POWER 7
 #define SAMPLE_SIZE 30
 
-double benchmark(int size){
-	int *token;
+double benchmark(CHAR size){
+	char[] token = char[size];
 	double startTime;
 	double endTime;
 	double executionTime;
-	int numProcesses = 2;
-	int myRank;
+	CHAR numProcesses = 2;
+	CHAR myRank;
 	MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
 	
 	startTime = MPI_Wtime();
 	if(myRank == 0){
-		MPI_Send(&token, size, MPI_INT, 1, 0, MPI_COMM_WORLD);
-		MPI_Recv(&token, size, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Send(&token, size, MPI_CHAR, 1, 0, MPI_COMM_WORLD);
+		MPI_Recv(&token, size, MPI_CHAR, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 	} else {
-		MPI_Recv(&token, size, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		MPI_Send(&token, size, MPI_INT, 0, 0, MPI_COMM_WORLD);
+		MPI_Recv(&token, size, MPI_CHAR, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		MPI_Send(&token, size, MPI_CHAR, 0, 0, MPI_COMM_WORLD);
 	}
 	endTime = MPI_Wtime();
 	executionTime = endTime - startTime;
 	return executionTime;
 }
 
-int main(int argc, char * argv[]){
-	int size = 1;
+CHAR main(CHAR argc, char * argv[]){
+	CHAR size = 1;
 	double time;
 	MPI_Init(&argc, &argv);
-	for(int i = 0 ; i < MAX_POWER ; i++){
+	for(CHAR i = 0 ; i < MAX_POWER ; i++){
 		size *= 10;
-		for(int j = 0; j < SAMPLE_SIZE ; j++){
+		for(CHAR j = 0; j < SAMPLE_SIZE ; j++){
 			time = benchmark(size);
-			printf("%d %d %lf\n", j, size, time);
+			prCHARf("%d %d %lf\n", j, size, time);
 		}
 	}
 	MPI_Finalize();
