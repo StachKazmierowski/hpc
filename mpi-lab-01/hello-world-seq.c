b2c782e88e63a30947c7f37643aba507a023e4f8
Stach <staho.kazmierowski@gmail.com>
2020-04-10 15:19:56 +0200

    upgrade of hello-world

--- mpi-lab-01/hello-world-seq.c
+++ mpi-lab-01/hello-world-seq.c
@@ -7,16 +7,23 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <time.h>
+#define MASTER 0
 
 int main(int argc, char * argv[])
 {
+	int numProcesses, myRank;
+	MPI_Init(&argc, &argv);
+	MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
+	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
     
     struct timespec spec;
-    clock_gettime(CLOCK_REALTIME, &spec);
-    srand(spec.tv_nsec); // use nsec to have a different value across different processes
+//    clock_gettime(CLOCK_REALTIME, &spec);
+//    srand(spec.tv_nsec); // use nsec to have a different value across different processes
     
-    unsigned t = rand() % 5;
-    sleep(t);
-    printf("Hello world from %d/%d (slept %u s)!\n", 0, 1, t);
+//    unsigned t = rand() % 5;
+//    sleep(t);
+    printf("Hello from thread number %d", myRank);
+    if(myRank == MASTER)
+    	printf("MASTER: number of tasks is: %d", numProcesses);
     return 0;
 }
