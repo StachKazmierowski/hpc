dd0fc3a2c1d83cbf7e41e83517fcff20b96633fb
Stach <staho.kazmierowski@gmail.com>
2020-04-10 16:29:34 +0200

    fixing ring.c

--- mpi-lab-01/ring.c
+++ mpi-lab-01/ring.c
@@ -17,10 +17,11 @@ int main(int argc, char * argv[])
 	if(myRank != 0){
 		MPI_Recv(&token, 1, MPI_INT, myRank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		printf("Process %d received token %d form process %d\n", myRank, token, myRank - 1);
-	}
 	token *= myRank;
+	}
 	MPI_Send(&token, 1, MPI_INT, (myRank + 1) % numProcesses , 0, MPI_COMM_WORLD);
-	
+	printf("Process %d send token %d to process %d\n", myRank, token, (myRank + 1) % numProcesses);
+			
 	if(myRank == 0){
 	MPI_Recv(&token, 1, MPI_INT, numProcesses - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		printf("MASTER received token %d form process %d\n", token, myRank - 1);
