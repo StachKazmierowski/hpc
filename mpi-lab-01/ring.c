6d843fca393fd525ac015fdd7916bdd72768dacf
Stach <staho.kazmierowski@gmail.com>
2020-05-06 19:54:19 +0200

    adding lab 4

--- mpi-lab-01/ring.c
+++ mpi-lab-01/ring.c
@@ -8,7 +8,7 @@
 int main(int argc, char * argv[])
 {
 	int numProcesses, myRank;
-	long token;
+	unsigned long token;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &numProcesses);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myRank);
