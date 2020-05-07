9e766683a5c1abc439cd5e537dcc421bba025ca1
Stach <staho.kazmierowski@gmail.com>
2020-05-07 16:00:08 +0200

    update

--- mpi-lab-04/blas-dmmmult.cpp
+++ mpi-lab-04/blas-dmmmult.cpp
@@ -9,12 +9,13 @@
 #include <omp.h>
 
 void matrixMultiplication(double* A, double* B, double*C, int n){
+int i, j, k;
 #pragma omp parallel shared(a,b,c) private(i,j,k) 
    {
 #pragma omp for  schedule(static)
-	for (i=0; i<m; i=i+1){
-		for (j=0; j<n; j=j+1){
-         	for (k=0; k<p; k=k+1){
+	for (i=0; i<n; i++){
+		for (j=0; j<n; j++){
+         	for (k=0; k<n; k++){
 				C[i*n + j] += A[i*n + k] * B[k*n + j];
          	}
       	}
